e4eaf394f4368d219b3d948c45a2c084f200962e
Raibel Botta <raibelbotta@gmail.com>
2017-07-05 19:20:13 -0400

    add entities for store invoices

---
+++ src/AppBundle/Entity/Invoice.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Invoice
+ *
+ * @ORM\Table(name="invoice")
+ * @ORM\Entity
+ */
+class Invoice
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model_name", type="string", length=255)
+     */
+    private $modelName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="serial_number")
+     */
+    private $serialNumber;
+
+    /**
+     * @var Provider
+     *
+     * @ORM\ManyToOne(targetEntity="Provider")
+     * @ORM\JoinColumn(nullable=false, onDelete="cascade")
+     */
+    private $provider;
+
+    /**
+     * @var Driver
+     *
+     * @ORM\ManyToOne(targetEntity="Driver")
+     * @ORM\JoinColumn(nullable=false, onDelete="cascade")
+     */
+    private $driver;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="InvoiceLine", mappedBy="invoice", cascade={"persist", "remove"})
+     */
+    private $lines;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="total_charge", type="decimal", precision=10, scale=2)
+     */
+    private $totalCharge;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->lines = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Invoice
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set modelName
+     *
+     * @param string $modelName
+     * @return Invoice
+     */
+    public function setModelName($modelName)
+    {
+        $this->modelName = $modelName;
+
+        return $this;
+    }
+
+    /**
+     * Get modelName
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return $this->modelName;
+    }
+
+    /**
+     * Set serialNumber
+     *
+     * @param string $serialNumber
+     * @return Invoice
+     */
+    public function setSerialNumber($serialNumber)
+    {
+        $this->serialNumber = $serialNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get serialNumber
+     *
+     * @return string
+     */
+    public function getSerialNumber()
+    {
+        return $this->serialNumber;
+    }
+
+    /**
+     * Set totalCharge
+     *
+     * @param string $totalCharge
+     * @return Invoice
+     */
+    public function setTotalCharge($totalCharge)
+    {
+        $this->totalCharge = $totalCharge;
+
+        return $this;
+    }
+
+    /**
+     * Get totalCharge
+     *
+     * @return string
+     */
+    public function getTotalCharge()
+    {
+        return $this->totalCharge;
+    }
+
+    /**
+     * Set provider
+     *
+     * @param \AppBundle\Entity\Provider $provider
+     * @return Invoice
+     */
+    public function setProvider(\AppBundle\Entity\Provider $provider)
+    {
+        $this->provider = $provider;
+
+        return $this;
+    }
+
+    /**
+     * Get provider
+     *
+     * @return \AppBundle\Entity\Provider
+     */
+    public function getProvider()
+    {
+        return $this->provider;
+    }
+
+    /**
+     * Set driver
+     *
+     * @param \AppBundle\Entity\Driver $driver
+     * @return Invoice
+     */
+    public function setDriver(\AppBundle\Entity\Driver $driver)
+    {
+        $this->driver = $driver;
+
+        return $this;
+    }
+
+    /**
+     * Get driver
+     *
+     * @return \AppBundle\Entity\Driver
+     */
+    public function getDriver()
+    {
+        return $this->driver;
+    }
+
+    /**
+     * Add lines
+     *
+     * @param \AppBundle\Entity\InvoiceLine $lines
+     * @return Invoice
+     */
+    public function addLine(\AppBundle\Entity\InvoiceLine $lines)
+    {
+        $this->lines[] = $lines;
+
+        return $this;
+    }
+
+    /**
+     * Remove lines
+     *
+     * @param \AppBundle\Entity\InvoiceLine $lines
+     */
+    public function removeLine(\AppBundle\Entity\InvoiceLine $lines)
+    {
+        $this->lines->removeElement($lines);
+    }
+
+    /**
+     * Get lines
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getLines()
+    {
+        return $this->lines;
+    }
+}
---
+++ src/AppBundle/Entity/InvoiceLine.php
@@ -0,0 +1,340 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * InvoiceLine
+ *
+ * @ORM\Table(name="invoice_line")
+ * @ORM\Entity
+ */
+class InvoiceLine
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var Invoice
+     *
+     * @ORM\ManyToOne(targetEntity="Invoice", inversedBy="lines")
+     * @ORM\JoinColumn(nullable=false, onDelete="cascade")
+     */
+    private $invoice;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="service_name")
+     * @Assert\Length(max=255)
+     */
+    private $serviceName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="clients_name", type="text", nullable=true)
+     * @Assert\Length(max=255)
+     */
+    private $clientsName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="client_reference", length=50, nullable=true)
+     * @Assert\Length(max=50)
+     */
+    private $clientReference;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="service_serial_number", length=20)
+     * @Assert\Length(max=20)
+     */
+    private $serviceSerialNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="meassurement_unit", type="string", length=49, nullable=true)
+     */
+    private $meassurementUnit;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer", nullable=true)
+     */
+    private $quantity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="unit_price", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $unitPrice;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="total_price", type="decimal", precision=10, scale=2)
+     */
+    private $totalPrice;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="notes", type="text", nullable=true)
+     */
+    private $notes;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set meassurementUnit
+     *
+     * @param string $meassurementUnit
+     * @return InvoiceLine
+     */
+    public function setMeassurementUnit($meassurementUnit)
+    {
+        $this->meassurementUnit = $meassurementUnit;
+
+        return $this;
+    }
+
+    /**
+     * Get meassurementUnit
+     *
+     * @return string
+     */
+    public function getMeassurementUnit()
+    {
+        return $this->meassurementUnit;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     * @return InvoiceLine
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set unitPrice
+     *
+     * @param string $unitPrice
+     * @return InvoiceLine
+     */
+    public function setUnitPrice($unitPrice)
+    {
+        $this->unitPrice = $unitPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get unitPrice
+     *
+     * @return string
+     */
+    public function getUnitPrice()
+    {
+        return $this->unitPrice;
+    }
+
+    /**
+     * Set totalPrice
+     *
+     * @param string $totalPrice
+     * @return InvoiceLine
+     */
+    public function setTotalPrice($totalPrice)
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get totalPrice
+     *
+     * @return string
+     */
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param string $notes
+     * @return InvoiceLine
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return string
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+
+    /**
+     * Set invoice
+     *
+     * @param \AppBundle\Entity\Invoice $invoice
+     * @return InvoiceLine
+     */
+    public function setInvoice(\AppBundle\Entity\Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+
+        return $this;
+    }
+
+    /**
+     * Get invoice
+     *
+     * @return \AppBundle\Entity\Invoice
+     */
+    public function getInvoice()
+    {
+        return $this->invoice;
+    }
+
+    /**
+     * Set serviceName
+     *
+     * @param string $serviceName
+     * @return InvoiceLine
+     */
+    public function setServiceName($serviceName)
+    {
+        $this->serviceName = $serviceName;
+
+        return $this;
+    }
+
+    /**
+     * Get serviceName
+     *
+     * @return string
+     */
+    public function getServiceName()
+    {
+        return $this->serviceName;
+    }
+
+    /**
+     * Set clientsName
+     *
+     * @param string $clientsName
+     * @return InvoiceLine
+     */
+    public function setClientsName($clientsName)
+    {
+        $this->clientsName = $clientsName;
+
+        return $this;
+    }
+
+    /**
+     * Get clientsName
+     *
+     * @return string
+     */
+    public function getClientsName()
+    {
+        return $this->clientsName;
+    }
+
+    /**
+     * Set clientReference
+     *
+     * @param string $clientReference
+     * @return InvoiceLine
+     */
+    public function setClientReference($clientReference)
+    {
+        $this->clientReference = $clientReference;
+
+        return $this;
+    }
+
+    /**
+     * Get clientReference
+     *
+     * @return string
+     */
+    public function getClientReference()
+    {
+        return $this->clientReference;
+    }
+
+    /**
+     * Set serviceSerialNumber
+     *
+     * @param string $serviceSerialNumber
+     * @return InvoiceLine
+     */
+    public function setServiceSerialNumber($serviceSerialNumber)
+    {
+        $this->serviceSerialNumber = $serviceSerialNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get serviceSerialNumber
+     *
+     * @return string
+     */
+    public function getServiceSerialNumber()
+    {
+        return $this->serviceSerialNumber;
+    }
+}
